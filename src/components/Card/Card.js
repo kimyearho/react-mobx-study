783bbd757bd6038adfda4c51e20e53b91345bb61
K ken <kstory8715@gmail.com>
2019-05-20 23:09:58 +0900

    .

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,18 +5,10 @@ import { withStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import FolderIcon from "@material-ui/icons/Folder";
-import DeleteIcon from "@material-ui/icons/Delete";
 import { inject, observer } from "mobx-react";
 
 const styles = {
@@ -56,7 +48,7 @@ class MediaCard extends Component {
                     <FolderIcon />
                   </Avatar>
                 </ListItemAvatar>
-                <ListItemText primary={data.snippet.title} />
+                <ListItemText onClick={this.detail} primary={data.snippet.title} />
               </ListItem>
             </List>
           </div>
